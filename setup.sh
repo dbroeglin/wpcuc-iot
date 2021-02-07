f3448fd027af1825c527d1628049d7fb3b7e705b
Dominique Broeglin <dominique.broeglin@gmail.com>
2021-02-07 19:59:11 +0100

    Added setup.sh based on https://docs.microsoft.com/en-us/azure/iot-hub/tutorial-routing

---
+++ setup.sh
@@ -0,0 +1,82 @@
+# This retrieves the subscription id of the account 
+#   in which you're logged in.
+# This field is used to set up the routing queries.
+subscriptionID=$(az account show --query id)
+
+# Concatenate this number onto the resources that have to be globally unique.
+# You can set this to "" or to a specific value if you don't want it to be random.
+# This retrieves a random value.
+randomValue=$RANDOM
+
+# Set the values for the resource names that 
+#   don't have to be globally unique.
+location=westus
+resourceGroup=ContosoResources
+iotHubConsumerGroup=ContosoConsumers
+containerName=contosoresults
+
+# Create the resource group to be used
+#   for all the resources for this tutorial.
+az group create --name $resourceGroup \
+    --location $location
+
+# The IoT hub name must be globally unique, 
+#   so add a random value to the end.
+iotHubName=ContosoTestHub$randomValue 
+echo "IoT hub name = " $iotHubName
+
+# Create the IoT hub.
+az iot hub create --name $iotHubName \
+    --resource-group $resourceGroup \
+    --sku S1 --location $location
+
+# Add a consumer group to the IoT hub for the 'events' endpoint.
+az iot hub consumer-group create --hub-name $iotHubName \
+    --name $iotHubConsumerGroup
+
+# The storage account name must be globally unique, 
+#   so add a random value to the end.
+storageAccountName=contosostorage$randomValue
+echo "Storage account name = " $storageAccountName
+
+# Create the storage account to be used as a routing destination.
+az storage account create --name $storageAccountName \
+    --resource-group $resourceGroup \
+    --location $location \
+    --sku Standard_LRS
+
+# Get the primary storage account key. 
+#    You need this to create the container.
+storageAccountKey=$(az storage account keys list \
+    --resource-group $resourceGroup \
+    --account-name $storageAccountName \
+    --query "[0].value" | tr -d '"') 
+
+# See the value of the storage account key.
+echo "storage account key = " $storageAccountKey
+
+# Create the container in the storage account. 
+az storage container create --name $containerName \
+    --account-name $storageAccountName \
+    --account-key $storageAccountKey \
+    --public-access off
+
+# The Service Bus namespace must be globally unique, 
+#   so add a random value to the end.
+sbNamespace=ContosoSBNamespace$randomValue
+echo "Service Bus namespace = " $sbNamespace
+
+# Create the Service Bus namespace.
+az servicebus namespace create --resource-group $resourceGroup \
+    --name $sbNamespace \
+    --location $location
+
+# The Service Bus queue name must be globally unique, 
+#   so add a random value to the end.
+sbQueueName=ContosoSBQueue$randomValue
+echo "Service Bus queue name = " $sbQueueName
+
+# Create the Service Bus queue to be used as a routing destination.
+az servicebus queue create --name $sbQueueName \
+    --namespace-name $sbNamespace \
+    --resource-group $resourceGroup
