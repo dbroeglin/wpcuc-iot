ff6ced6f36e9267ad1b0736870d23bfeb0187fa2
Dominique Broeglin <dominique.broeglin@gmail.com>
2021-02-07 19:58:45 +0100

    Added script that sends WPCUC data to Azure IoT

---
+++ sender.py
@@ -0,0 +1,19 @@
+import json
+import os
+import time
+
+from azure.iot.device import IoTHubDeviceClient, Message
+
+JSON_FILE = os.getenv('JSON_FILE', '/var/run/wpcuc.json')
+CONNECTION_STRING = os.getenv('CONNECTION_STRING')
+if not(CONNECTION_STRING and CONNECTION_STRING.strip()):
+    raise "Could not find CONNECTION_STRING environment variable"
+
+
+client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING) 
+
+with open(JSON_FILE) as file:
+    data = file.read()
+    message = Message(data)
+    print( "Sending message: {}".format(message) )
+    client.send_message(message)
